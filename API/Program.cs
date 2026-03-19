921aff3c74476bd0f59369e24019b519636935f8
chelsea424 <wangchelsea96@gmail.com>
2026-03-20 00:12:56 +1030

    Implement product repository pattern and update ProductsController for improved data handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
+using Core.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 //above this line is service configuration
 var app = builder.Build();
 
@@ -19,5 +22,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
+
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
 
 app.Run();
--- Infrastructure/Config/ProductConfiguration.cs
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -8,6 +8,7 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
     public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
     {
-        builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+        builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+        builder.Property(x => x.Name).IsRequired();
     }
 }
