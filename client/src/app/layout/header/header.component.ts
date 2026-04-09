f2b7bdee1b92452fff85f439623d0054d28946a8
chelsea424 <wangchelsea96@gmail.com>
2026-04-09 23:01:14 +0930

    feat(shop): wire home and product detail navigation

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +11,9 @@ import {MatIcon} from '@angular/material/icon';
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
+    MatBadge,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
